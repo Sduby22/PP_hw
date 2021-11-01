aba3ed0757a519c1786278ab23e18863406f5a6f
osennyaya <supersduby@gmail.com>
2021-11-01 09:10:10 +0800

    feat(ConfigLoader): add default config.

--- src/ConfigLoader.py
+++ src/ConfigLoader.py
@@ -10,6 +10,12 @@ class ConfigLoader:
     def getConfig(self):
         return self._config
 
+    def _updateDefault(self):
+        with open('./src/data/default_config.yaml', 'r', encoding='utf-8') as f:
+            default = load(f, CLoader)
+        default.update(self._config)
+        self._config = default
+
     def load(self, path='./config.yaml', encoding='utf8'):
         with open(path, 'r', encoding=encoding) as f:
             config = load(f, CLoader)
@@ -17,6 +23,7 @@ class ConfigLoader:
             self._config = config
         else:
             self._config = None
+        self._updateDefault()
 
     def _validate(self, config):
         schema = Schema({
@@ -35,3 +42,12 @@ class ConfigLoader:
             }
         })
         return schema.validate(config)
+
+    def getRuntimeConfig(self):
+        return self._config.get('runtime')
+
+    def getJobConfig(self):
+        return self._config.get('job')
+
+    def getScriptsConfig(self):
+        return self._config.get('scripts')
