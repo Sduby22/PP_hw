51ddde4b4cf6c253155d312d489661425d55f400
osennyaya <supersduby@gmail.com>
2021-11-06 22:40:30 +0800

    runtime variables

--- src/Runtime.py
+++ src/Runtime.py
@@ -12,11 +12,25 @@ class Runtime:
         logger.debug(f"[Runtime] config is {self.getConfig()}")
         self._runpy = getInstance()
         self._runpy.init(self._configLoader)
+        self._variables = {
+                '_input': '',
+                '_input_number': '',
+                '_number': '',
+                '_input': '',
+                '_ret': '',
+                }
         pass
 
     def handleCall(self, number):
         logger.info(f'Incoming call {number}')
+        self._variables['number'] = number
         pass    
+
+    def _speak(self, str):
+        print(f'<<< {str}')
+
+    def _wait(self, time):
+        print(f'Waiting {time} milliseconds for user input...')
 
     def getConfig(self):
         return self._configLoader.getRuntimeConfig()
